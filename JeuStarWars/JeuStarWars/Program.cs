f640cf303393834df32d5e381176d2cfe5e3aace
RonanRIZZARDI <ronanrizzardi@gmail.com>
2020-05-13 18:29:06 +0200

    ok

--- JeuStarWars/JeuStarWars/Program.cs
+++ JeuStarWars/JeuStarWars/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 
-namespace JeuStarWars
+namespace JeuStarWars 
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello World!");
         }
     }
