8146453d9f83f8cc99d61b375923445a5612c545
chaimaa <kerboubchaimaa@gmail.com>
2020-05-15 06:53:26 +0200

    Mise en place de la grille (version 1)

--- JeuStarWars/DomainServices/Services/PnjService.cs
+++ JeuStarWars/DomainServices/Services/PnjService.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ApplicationCore.services.Services
+namespace ApplicationCore.services
 {
    public class PnjService: IPnjService
     {
         public PnjService() : base() { }
-        {
-            
-        }
+      
     }
 }
--- JeuStarWars/DomainServices/Services/PositionService.cs
+++ JeuStarWars/DomainServices/Services/PositionService.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ApplicationCore.services.Services
+namespace ApplicationCore.services
 {
     public class PositionService: IPositionService
     {
-        public PositionService() : base() { }
+        public PositionService() : base() 
         {
-
         }
+      
     }
 }
