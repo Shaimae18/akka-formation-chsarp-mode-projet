bbf89cb8d10bb67b7be806750ca0a62fbd2830c7
chaimaa <kerboubchaimaa@gmail.com>
2020-05-19 15:26:26 +0200

    .

--- JeuStarWars/ApplicationCore.Services/Services/GrilleService.cs
+++ JeuStarWars/ApplicationCore.Services/Services/GrilleService.cs
@@ -15,14 +15,14 @@ namespace ApplicationCore.services
                 new Position(2,9,currentJoueur),
                 new Position(1,6, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
                 new Position(3,4, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
-                //new Position(10,2, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
-                //new Position(6,9, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
-                //new Position(7,2, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant ,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
-                //new Position(9,3, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant ,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
-                //new Position(6,1, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
-                //new Position(5,4, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
-                //new Position(1,1, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant ,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
-                //new Position(0,10, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
+                new Position(10,2, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
+                new Position(6,9, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
+                new Position(7,2, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant ,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
+                new Position(9,3, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant ,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
+                new Position(6,1, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
+                new Position(5,4, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
+                new Position(1,1, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant ,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
+                new Position(1,10, new Joueur(){PointsVie=1,TypeJoueur= TypeJoueur.Adversaire,Etat=Etat.Vivant,PersonnageJoueur= new PersonnageJoueur(pseudo:"Adv1",cote:Cote.Obscur,typePersonnage:TypePersonnage.NonLanceurDeSort)}),
             };
         }
     }
